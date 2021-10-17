cbdda26c02ef2f2bb535f8d6aef50235fa005dfe
gpalat3 <gpalat3@gatech.edu>
2021-10-17 12:41:49 -0700

    Code updates

--- HW2/FourPeaksGridSearch.py
+++ HW2/FourPeaksGridSearch.py
@@ -18,7 +18,7 @@ def randomHillClimb(fitness_fn, problem_size):
     times = np.array(times)
     problem = mlrose_hiive.DiscreteOpt(length=100, fitness_fn=fitness_fn, maximize=True, max_val=2)
     rhc = mlrose_hiive.RHCRunner(problem=problem, iteration_list=[1000], max_attempts=100, seed=99, restart_list=[0],
-                                 experiment_name='RandomizedHillClimbing', output_directory='output/FlipFlop/')
+                                 experiment_name='RandomizedHillClimbing', output_directory='output/FourPeaks/')
     rhc_stats, rhc_curves = rhc.run()
     return fitness, times, rhc_stats, rhc_curves
 
@@ -38,7 +38,7 @@ def simulatedAnnealing(fitness_fn, problem_size):
     problem = mlrose_hiive.DiscreteOpt(length=100, fitness_fn=fitness_fn, maximize=True, max_val=2)
     sa = mlrose_hiive.SARunner(problem=problem, iteration_list=[1000], max_attempts=100, temperature_list=[250],
                                decay_list=[mlrose_hiive.ExpDecay], seed=99, experiment_name='SimulatedAnnealing',
-                               output_directory='output/FlipFlop/')
+                               output_directory='output/FourPeaks/')
     sa_stats, sa_curves = sa.run()
     return fitness, times, sa_stats, sa_curves
 
@@ -58,7 +58,7 @@ def geneticAlg(fitness_fn, problem_size):
     problem = mlrose_hiive.DiscreteOpt(length=100, fitness_fn=fitness_fn, maximize=True, max_val=2)
     ga = mlrose_hiive.GARunner(problem=problem, iteration_list=[1000], max_attempts=100, population_sizes=[200],
                                mutation_rates=[0.2], seed=99, experiment_name='GeneticAlgorithm',
-                               output_directory='output/FlipFlop/')
+                               output_directory='output/FourPeaks/')
     ga_stats, ga_curves = ga.run()
     return fitness, times, ga_stats, ga_curves
 
@@ -78,7 +78,7 @@ def mimic(fitness_fn, problem_size):
     problem = mlrose_hiive.DiscreteOpt(length=100, fitness_fn=fitness_fn, maximize=True, max_val=2)
     mimic = mlrose_hiive.MIMICRunner(problem=problem, iteration_list=[1000], max_attempts=100, population_sizes=[200],
                                      keep_percent_list=[0.25], use_fast_mimic=True, seed=99, experiment_name='MIMIC',
-                                     output_directory='output/FlipFlop/')
+                                     output_directory='output/FourPeaks/')
     mimic_stats, mimic_curves = mimic.run()
     return fitness, times, mimic_stats, mimic_curves
 
@@ -91,15 +91,6 @@ def plotCurves(x1, y1, y2, y3, y4, title, xlabel, ylabel, savefile):
     plt.plot(x1, y2, 'o-', label='SA')
     plt.plot(x1, y3, 'o-', label='GA')
     plt.plot(x1, y4, 'o-', label='MIMIC')
-    plt.legend(loc='best')
-    plt.savefig(savefile)
-
-def plotIndCurves(x, y, title, xlabel, ylabel, label, savefile):
-    plt.figure()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.plot(x, y, 'o-', label=label)
     plt.legend(loc='best')
     plt.savefig(savefile)
 
@@ -116,32 +107,22 @@ def plotRunnerCurves(x1, y1, x2, y2, x3, y3, x4, y4, title, xlabel, ylabel, save
     plt.savefig(savefile)
 
 if __name__ == '__main__':
-    fitness_fn = mlrose_hiive.FlipFlop()
-    problem_size = range(10, 201, 10)
+    fitness_fn = mlrose_hiive.FourPeaks(t_pct=0.1)
+    problem_size = range(10, 101, 10)
     fitness_rhc, times_rhc, stats_rhc, curves_rhc = randomHillClimb(fitness_fn, problem_size)
     fitness_sa, times_sa, stats_sa, curves_sa = simulatedAnnealing(fitness_fn, problem_size)
     fitness_ga, times_ga, stats_ga, curves_ga = geneticAlg(fitness_fn, problem_size)
     fitness_mimic, times_mimic, stats_mimic, curves_mimic = mimic(fitness_fn, problem_size)
-    title, xlabel, ylabel = ['FlipFlop - Fitness', 'Problem Size', 'Fitness']
-    savefile = 'plots/FlipFlop_Fitness_Problem_Size.png'
+    title, xlabel, ylabel = ['FourPeaks - Fitness', 'Problem Size', 'Fitness']
+    savefile = 'plots/FourPeaks_Fitness_Problem_Size.png'
     plotCurves(problem_size, fitness_rhc, fitness_sa, fitness_ga, fitness_mimic, title, xlabel, ylabel, savefile)
-    label = 'RHC'
-    savefile = 'plots/FlipFlop_Fitness_Problem_Size_' + label + '.png'
-    plotIndCurves(problem_size, fitness_rhc, title, xlabel, ylabel, label, savefile)
-    label = 'SA'
-    savefile = 'plots/FlipFlop_Fitness_Problem_Size_' + label + '.png'
-    plotIndCurves(problem_size, fitness_sa, title, xlabel, ylabel, label, savefile)
-    label = 'GA'
-    savefile = 'plots/FlipFlop_Fitness_Problem_Size_' + label + '.png'
-    plotIndCurves(problem_size, fitness_ga, title, xlabel, ylabel, label, savefile)
-    label = 'MIMIC'
-    savefile = 'plots/FlipFlop_Fitness_Problem_Size_' + label + '.png'
-    plotIndCurves(problem_size, fitness_mimic, title, xlabel, ylabel, label, savefile)
-    title, xlabel, ylabel = ['FlipFlop - Time', 'Problem Size', 'Time']
-    savefile = 'plots/FlipFlop_Time_Problem_Size.png'
+    title, xlabel, ylabel = ['FourPeaks - Time', 'Problem Size', 'Time']
+    savefile = 'plots/FourPeaks_Time_Problem_Size.png'
     plotCurves(problem_size, times_rhc, times_sa, times_ga, times_mimic, title, xlabel, ylabel, savefile)
-    title, xlabel, ylabel = ['FlipFlop - FEvals', 'Iteration', 'FEvals']
-    savefile = 'plots/FlipFlop_Iterations_FEvals.png'
+    title, xlabel, ylabel = ['FourPeaks - FEvals', 'Iteration', 'FEvals']
+    savefile = 'plots/FourPeaks_Iterations_FEvals.png'
     plotRunnerCurves(curves_rhc[xlabel], curves_rhc[ylabel], curves_sa[xlabel], curves_sa[ylabel], curves_ga[xlabel],
                curves_ga[ylabel], curves_mimic[xlabel], curves_mimic[ylabel], title, xlabel, ylabel, savefile)
+
+
 
